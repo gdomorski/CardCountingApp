a04262d0b098b0393e6f9b7b0d258f7a88c78951
Greg Domorski <greg.domorski@gmail.com>
2015-07-02 19:03:33 -0400

    Update to JS File

--- yeomanApp/app/scripts/main.js
+++ yeomanApp/app/scripts/main.js
@@ -5,14 +5,15 @@ function cardCounter(count){
 
     var rnd = Math.floor(Math.random() * allCards.length);
     
-    var allCards = ['1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', "AC"];
+    var allCards = ['1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', "AC", '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', "AS"];
     var minusOne = ['10C', 'JC', 'QC', 'KC', 'AC'];
     var plusOne = ['1C','2C','3C','4C','5C','6C'];
         
-    for(var i = 0; i < arr.length; i++){
-        if(plusOne.indexOf(allcards[i]) !== -1){
+    var randomCard = allCards[rnd];    
+    
+        if(plusOne.indexOf(randomCard) !== -1){
          count++;
-        }else if(minusOne.indexOf(arr[i]) !== -1){
+        }else if(minusOne.indexOf(randomCard) !== -1){
          count--;   
         }
     }
