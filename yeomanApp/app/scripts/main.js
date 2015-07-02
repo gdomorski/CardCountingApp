f992725746f8269aabf80d090f6be04e129be095
Greg Domorski <greg.domorski@gmail.com>
2015-07-02 19:23:16 -0400

    New comments

--- yeomanApp/app/scripts/main.js
+++ yeomanApp/app/scripts/main.js
@@ -1,35 +1,46 @@
 // jshint devel:true
 
+//list all the possible cards in a 52 yard deck
+var allCards = ['1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', "AC", '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', "AS", '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', "AD", '1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',];
 
 function cardCounter(count){
 
+
+    //find a random number for the index
     var rnd = Math.floor(Math.random() * allCards.length);
-    
-    var allCards = ['1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', "AC", '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', "AS"];
+    //assign the appropiate cards as minus 1
     var minusOne = ['10C', 'JC', 'QC', 'KC', 'AC'];
+    //assign the appropiate cards as plus 1
     var plusOne = ['1C','2C','3C','4C','5C','6C'];
-        
+    
+    //get a random card value
     var randomCard = allCards[rnd];    
     
+        //if is is plus one, add to the count
         if(plusOne.indexOf(randomCard) !== -1){
          count++;
+        //or else if it is negative one, subtract from the count
         }else if(minusOne.indexOf(randomCard) !== -1){
          count--;   
         }
     }
+    //change the count as text
     countText.textContent = 'The Current Count is '+ count;
-    showImage();
+    //show the image of the card you selected
+    showImage(randomCard);
+    //take it out of the deck so you don't get the same card twice in a 52 card deck
+    allCards.splice(rnd, 1)
 }
 
 
 function showImage(randomCard) {
 
-
-    
+    //create an image element
     var img = document.createElement("img");
     img.src = 'images/cardimages/' + randomCard + '.png';
     img.width = 250;
     img.height = 363;
+    //add that image to the page
     document.body.appendChild(img);
     
 }
