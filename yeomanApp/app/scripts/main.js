61f8368bfcb60b4781165811d38aca8fbeabcebf
Greg Domorski <greg.domorski@gmail.com>
2015-07-02 21:11:41 -0400

    New Features

--- yeomanApp/app/scripts/main.js
+++ yeomanApp/app/scripts/main.js
@@ -1,7 +1,7 @@
 // jshint devel:true
 
 //list all the possible cards in a 52 yard deck
-var allCards = ['1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH'];
+var allCards = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH'];
 var count = 0;
 
 function cardCounter(){
@@ -12,7 +12,7 @@ function cardCounter(){
     //assign the appropiate cards as minus 1
     var minusOne = ['10C', 'JC', 'QC', 'KC', 'AC', '10S', 'JS', 'QS', 'KS', 'AS', '10D', 'JD', 'QD', 'KD', 'AD', '10H', 'JH', 'QH', 'KH', 'AH'];
     //assign the appropiate cards as plus 1
-    var plusOne = ['1C','2C','3C','4C','5C','6C', '1S', '2S', '3S', '4S', '5S', '6S', '1D', '2D', '3D', '4D', '5D', '6D', '1H', '2H', '3H', '4H', '5H', '6H'];
+    var plusOne = ['2C','3C','4C','5C','6C', '2S', '3S', '4S', '5S', '6S', '2D', '3D', '4D', '5D', '6D', '1H', '2H', '3H', '4H', '5H', '6H'];
     
     //get a random card value
     var randomCard = allCards[rnd];    
@@ -26,7 +26,11 @@ function cardCounter(){
         }
     
     //change the count as text
-    countText.textContent = 'The Current Count is '+ count;
+    if(count > 0){
+        countText.textContent = 'The count is +'+ count;
+    }else{
+        countText.textContent = 'The count is '+ count;
+    }
     //show the image of the card you selected
     showImage(randomCard);
     //take it out of the deck so you don't get the same card twice in a 52 card deck
@@ -37,7 +41,9 @@ function cardCounter(){
 function hideIntroBox(){
     introBox.style.display = "none";
     dealerButton.style.display = "block";
-
+    countText.style.display = "block";
+    resetGame.style.display = "block";
+    buttonstyles.style.display ="block";
 }
 
 
